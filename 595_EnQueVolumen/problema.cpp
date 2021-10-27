5f974881bc6fcdbf62dd073840f9b8879aa8d568
Nicolás <nabasolo3@gmail.com>
2021-10-27 11:21:36 -0300

    595

---
+++ 595_EnQueVolumen/problema.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  int ncasos;
+  string programa;
+
+  cin >> ncasos;
+  for (int i = 0; i < ncasos ; ++i){
+    cin >> programa;
+   
+    cout << programa[0] << "\n";
+  }
+  
+  return 0;
+}
