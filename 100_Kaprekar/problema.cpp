e90b9eb70cfa3281d1c987dfef6fc7952aa8d693
Nicolás <nabasolo3@gmail.com>
2022-01-14 10:07:39 -0300

    156_Ascensor
    156_Ascensor

--- 100_Kaprekar/problema.cpp
+++ 100_Kaprekar/problema.cpp
@@ -1,51 +1,61 @@
-#include <iostream>     // std::cout
+#include <iostream> // std::cout
 #include <algorithm>
 #include <stdlib.h>
 using namespace std;
 
-bool myfunctionMayor (int i,int j) { return (i>j); }
+bool myfunctionMayor(int i, int j) { return (i > j); }
 
-string mayor(string& numero){
-	sort(numero.begin(),numero.end(),myfunctionMayor);
+string mayor(string &numero)
+{
+	sort(numero.begin(), numero.end(), myfunctionMayor);
 	return numero;
 }
 
-string menor(string& numero){
-	sort(numero.begin(),numero.end());
+string menor(string &numero)
+{
+	sort(numero.begin(), numero.end());
 	return numero;
 }
 
-int resolver(string numero, int cant){
-	
-	if (numero=="6174") return cant;
-	
-	int resta,Imayor,Imenor;
+int resolver(string numero, int cant)
+{
+
+	if (numero == "6174")
+		return cant;
+
+	int resta, Imayor, Imenor;
 	string Nmayor, Nmenor;
-	
-	Nmayor=mayor(numero);
-	Nmenor=menor(numero);
-	
+
+	Nmayor = mayor(numero);
+	Nmenor = menor(numero);
+
 	Imayor = stoi(Nmayor);
-	if (Imayor>9999) return cant;
 	Imenor = stoi(Nmenor);
-	resta=Imayor-Imenor;
-	if(resta==0) return 8;
-	while(resta<1000){
-		resta=resta*10;
+	resta = Imayor - Imenor;
+	if (resta == 0)
+		return 8;
+	while (resta < 1000)
+	{
+		resta = resta * 10;
 	}
-	
-	return (resolver(to_string(resta),cant+1));
+
+	return (resolver(to_string(resta), cant + 1));
 }
 
-int main(){
-	int casos,i;
-	string numero;
+int main()
+{
+	int casos, i, numero;
 	cin >> casos;
-	
-	for(i=0;i<casos;i++){
-		cin >> numero;	
-		cout << resolver(numero,0) << "\n";		
+
+	for (i = 0; i < casos; ++i)
+	{
+		cin >> numero;
+		while (0 < numero < 1000)
+		{
+			numero = numero * 10;
+		}
+		cout << resolver(to_string(numero), 0) << "\n";
 	}
-			
-   	return 0;
+
+	return 0;
 }
---
+++ 156_Ascensor/problema.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int pisoActual, pisoDestino, longitud;
+  longitud = 0;
+
+  cin >> pisoActual;
+  while (pisoActual != -1)
+  {
+    cin >> pisoDestino;
+    while (pisoDestino != -1)
+    {
+      longitud = longitud + abs(pisoActual - pisoDestino);
+      pisoActual = pisoDestino;
+      cin >> pisoDestino;
+    }
+    cout << longitud << "\n";
+    longitud = 0;
+    cin >> pisoActual;
+  }
+
+  return 0;
+}
