87cd4eac1b2e3b234665f544356eaf3c8e84f855
Nicolás <nabasolo3@gmail.com>
2022-02-10 16:13:13 -0300

    585

--- 585_RelojProgramable/problema.cpp
+++ 585_RelojProgramable/problema.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
+#include <iomanip> //setfill, setw
+
 using namespace std;
+
+int solucion(double aguja1, double aguja2)
+{
+  double hour, minut;
+
+  minut = aguja2 / 6;
+  hour = (aguja1 - (minut / 2)) / 30;
+
+  if (int(hour) == hour && int(minut) == minut)
+  {
+    cout << setfill('0') << setw(2) << hour << ":";
+    cout << setfill('0') << setw(2) << minut << "\n";
+    return 1;
+  }
+
+  return 0;
+}
 
 int main()
 {
   int ncasos;
-  float aguja1, aguja2;
+  double aguja1, aguja2;
   cin >> ncasos;
 
   for (int i = 0; i < ncasos; ++i)
   {
-    cin >> aguja1;
-    cin >> aguja2;
+    cin >> aguja1 >> aguja2;
 
-    cout << int(aguja1 / 30) << ":" << int(aguja2 / 6) << "\n";
+    if (!solucion(aguja1, aguja2))
+    {
+      if (!solucion(aguja2, aguja1))
+      {
+        cout << "ERROR\n";
+      }
+    }
   }
 
   return 0;
